15de4d36e34d9503ca7b1e0b26be174bdacf9265
Trường Chinh <156447398+trgchinhh@users.noreply.github.com>
2025-04-06 14:10:29 +0700

    Add files via upload

---
+++ Lib_C_healthy/build/lib/C_healthy/__init__.py
@@ -0,0 +1,4 @@
+from .C_healthy import BMI, BMR, TDEE, HDSD, WHR, LBM, BFP, BBW, IBW, MA, FFMI, RFM, VFR, BSA, VO2MAX, HSI, MMI, BFM
+
+__version__ = "0.2.5"
+__all__ = ["BMI", "BMR", "TDEE", "HDSD", "WHR", "LBM", "BFP", "BBW", "IBW", "MA", "FFMI", "RFM", "VFR", "BSA", "VO2MAX", "HSI", "MMI", "BFM"]
